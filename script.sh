0c0a7ac564088543761c28624b297082f0202b62
loicNorgeot <loic.norgeot@gmail.com>
2015-07-20 16:40:50 +0200

    Allows for a color triangle, seen from perspective

---
+++ script.sh
@@ -0,0 +1,4 @@
+make clean
+make main
+sleep 0.1
+executables/./main
--- sources/main.cpp
+++ sources/main.cpp
@@ -6,101 +6,115 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h> 
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "shader.h"
 
 using namespace std;
+using namespace glm;
 
 int main(){
-
+  //Paramètres d'initialisation
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
 
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
-  GLFWwindow* window;
-
+  //Initialisation de GLFW
   if(!glfwInit()) { 
     cout << stderr <<  " Failed to initialize GLFW\n" << endl; 
     return 0; 
   }
 
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
+  //Création de la fenètre via GLFW
+  GLFWwindow* window;
   window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL); 
   if( window == NULL ){ 
-    cout << stderr << "Failed to open GLFW window" << endl;
+    cout << "Failed to open GLFW window" << endl;
     glfwTerminate(); 
     return 0; 
   } 
+  //Linkage au contexte OpenGL
   glfwMakeContextCurrent(window); 
 
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
+  //Initialisation de GLEW
   glewExperimental=GL_TRUE;
   GLenum err = glewInit();
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
   if (err!=GLEW_OK){ 
     cout << glewGetErrorString(err) << endl; 
     return 0; 
   }
   
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
+  //Création d'un VAO
   GLuint VertexArrayID; 
   glGenVertexArrays(1, &VertexArrayID); 
   glBindVertexArray(VertexArrayID);
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
 
-  // S'assure que l'on puisse capturer la touche échap utilisée plus bas
+  //Ecoute des inputs claviers
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
-
-
-
+  
   // Un tableau de trois vecteurs qui représentent trois sommets
-  static const GLfloat g_vertex_buffer_data[] = { 
-    -1.0f, -1.0f, 0.0f, 
-    1.0f, -1.0f, 0.0f, 
-    0.0f,  1.0f, 0.0f, 
-  };
+  static const GLfloat g_vertex_buffer_data[] = {-1.0f, -1.0f, 0.0f, 
+						 1.0f, -1.0f, 0.0f, 
+						 0.0f,  1.0f, 0.0f};
 
-  GLuint vertexbuffer; 
-  // Génère un tampon et place l'identifiant dans 'vertexbuffer'
-  glGenBuffers(1, &vertexbuffer); 
-  // Les commandes suivantes vont parler de notre tampon 'vertexbuffer'
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer); 
-  // Fournit les sommets à OpenGL.
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
- 
+  //Lecture des shaders, vert et frag
   GLuint programID = LoadShaders("shaders/SimpleVertexShader.vert",
 				 "shaders/SimpleFragmentShader.frag");
+  //Identifiant pour la matrice MVP
+  GLuint MatrixID = glGetUniformLocation(programID, "MVP"); 
 
-  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+  // Champ de vision de 45°, ration 4:3, distance d'affichage : 0.1 unités <-> 100 unités
+  glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f); 
+  glm::mat4 View = glm::lookAt(glm::vec3(4,0,3),   // Caméra est à (4,3,3)
+			       glm::vec3(0,0,0),   // Regarde l'origine
+			       glm::vec3(0,1,0));  // La tête vers le haut
+  // Matrice de modèle : ID ici, change pour chaque modèle 
+  glm::mat4 Model = glm::mat4(1.0f);
+  // ModelViewProjection : multiplication 
+  glm::mat4 MVP = Projection * View * Model; // Multiplication en sens inverse
 
+  //Envoi des données à OpenGL
+  GLuint vertexbuffer; 
+  glGenBuffers(1, &vertexbuffer); 
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer); 
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+  //Spécification d'une couleur de fond
+  glClearColor(0.1f, 0.1f, 0.15f, 0.0f);
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //BOUCLE PRINCIPALE
   while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && 
-	 glfwWindowShouldClose(window) == 0 ){
+	 glfwWindowShouldClose(window) == 0 ){//Tant que ni esc ni fermeture
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // Utilise notre shader
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//Nettoyage
     glUseProgram(programID);
-    // Dessine le triangle...
 
-    // premier tampon d'attributs : les sommets
+    //Transformation au shader actuel dans uniforme "MVP", pour chaque modèle en théorie
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    //Premier Tampon d'attributs : les sommets
     glEnableVertexAttribArray(0); 
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer); 
-    glVertexAttribPointer(0,                  // attribut 0. Aucune raison particulière pour 0, mais cela doit correspondre au « layout » dans le shader 
+    glVertexAttribPointer(0,                  // correspond au « layout » dans le shader 
 			  3,                  // taille
 			  GL_FLOAT,           // type 
 			  GL_FALSE,           // normalisé ? 
 			  0,                  // nombre d'octets séparant deux sommets dans le tampon
 			  (void*)0);          // Décalage du tableau de tampon
  
-    // Dessine le triangle ! 
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Démarre à partir du sommet 0; 3 sommets au total -> 1 triangle 
+    //Dessin
+    glDrawArrays(GL_TRIANGLES, 0, 3); //Du sommet 0, pour 3 sommets
  
+    //Fin et nettoyage
     glDisableVertexAttribArray(0);
     glUseProgram(0);
     glfwSwapBuffers(window);
   }
-
-  cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
   return 1;
 }
+
+
+//cout << "Err " << __LINE__ << ":\t" << glGetError() << endl;
